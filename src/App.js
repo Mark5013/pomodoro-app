d486e0602fc4e226736e805f552c5afe8ab614a6
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-07-20 11:04:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ErrorPage from "./components/Pages/ErrorPage";
+import LoginPage from "./components/Pages/LoginPage";
+import MainPage from "./components/Pages/MainPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
