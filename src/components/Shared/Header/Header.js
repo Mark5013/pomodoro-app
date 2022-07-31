e9237364bfe980dc58f440d9c759a17dde186597
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-07-30 20:27:19 -0400

    website now uses httponly cookies to persist user login

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useGoogleLogin } from "@react-oauth/google";
 import Button from "../UI/Button";
 import UserContext from "../../../store/userContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 function Header() {
 	const userCtx = useContext(UserContext);
@@ -11,21 +11,23 @@ function Header() {
 	const login = useGoogleLogin({
 		onSuccess: async (codeResponse) => {
 			let tokenResponse;
-			let tokens;
+			let accessToken;
 			try {
 				tokenResponse = await fetch("http://localhost:5000/login", {
 					method: "POST",
+					credentials: "include",
 					headers: {
 						"Content-type": "application/json",
 					},
 					body: JSON.stringify(codeResponse),
 				});
 				// tokens obtained from google api
-				tokens = await tokenResponse.json();
+				accessToken = await tokenResponse.json();
 			} catch (err) {
 				console.log(err);
 			}
 
+			// fetch users info from google acc
 			let userInfoResponse;
 			let userInfo;
 			try {
@@ -33,7 +35,7 @@ function Header() {
 					"https://www.googleapis.com/oauth2/v3/userinfo",
 					{
 						headers: {
-							Authorization: `Bearer ${tokens.access_token}`,
+							Authorization: `Bearer ${accessToken}`,
 						},
 					}
 				);
@@ -44,6 +46,7 @@ function Header() {
 				console.log(err);
 			}
 
+			// find or create google account
 			let userResponse;
 			let user;
 			try {
@@ -51,6 +54,7 @@ function Header() {
 					"http://localhost:5000/login/findOrCreate",
 					{
 						method: "POST",
+						credentials: "include",
 						headers: { "Content-type": "application/json" },
 						body: JSON.stringify({
 							name: userInfo.name,
@@ -73,6 +77,68 @@ function Header() {
 		},
 		flow: "auth-code",
 	});
+
+	useEffect(() => {
+		async function persistLogin() {
+			const response = await fetch(
+				"http://localhost:5000/login/refreshToken",
+				{
+					credentials: "include",
+				}
+			);
+
+			const token = await response.json();
+
+			let userInfoResponse;
+			let userInfo;
+			try {
+				userInfoResponse = await fetch(
+					"https://www.googleapis.com/oauth2/v3/userinfo",
+					{
+						headers: {
+							Authorization: `Bearer ${token.accessToken}`,
+						},
+					}
+				);
+
+				// google account user info
+				userInfo = await userInfoResponse.json();
+			} catch (err) {
+				console.log(err);
+			}
+
+			// find or create google account
+			let userResponse;
+			let user;
+			try {
+				userResponse = await fetch(
+					"http://localhost:5000/login/findOrCreate",
+					{
+						method: "POST",
+						credentials: "include",
+						headers: { "Content-type": "application/json" },
+						body: JSON.stringify({
+							name: userInfo.name,
+							id: userInfo.sub,
+						}),
+					}
+				);
+				// user created or found from data base
+				user = await userResponse.json();
+			} catch (err) {
+				console.log(err);
+			}
+			console.log(user);
+			// set user to logged in along with storing name and picture, move to database soon :)
+			if (user) {
+				userCtx.login(userInfo.name, userInfo.picture, userInfo.sub);
+			} else {
+				console.log(user);
+			}
+		}
+
+		persistLogin();
+	}, []);
 
 	return (
 		<div className={`${styles.header}`}>
